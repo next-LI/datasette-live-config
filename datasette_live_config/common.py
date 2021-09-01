a423d5891653d65fd561703bc05b96e87dd82db5
Brandon Roberts <brandon@bxroberts.org>
2021-08-31 17:10:15 -0700

    Fix for recursion: only use local metadata for plugin config

--- datasette_live_config/common.py
+++ datasette_live_config/common.py
@@ -7,7 +7,7 @@ TABLE_NAME=DB_NAME
 
 
 def get_db_path(datasette):
-    config = datasette.plugin_config("datasette-live-config") or {}
+    config = datasette._metadata_local.get("datasette-live-config") or {}
     default_db_path = config.get("db_path", DEFAULT_DBPATH)
     return os.path.join(default_db_path, f"{DB_NAME}.db")
 
